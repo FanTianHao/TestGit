a9f9f8817a60e97556e5d5c7d94a416bead399ea
fantianhao <DarrenFan07@163.com>
2019-06-15 18:24:51 +0800

    add one Test class

---
+++ ego-parent/ego-manager/ego-manager-web/src/main/java/com/ego/controller/TestGit.java
@@ -0,0 +1,10 @@
+package com.ego.controller;
+
+/**
+ * Created by ASUS on 2019/6/15.
+ */
+public class TestGit {
+    public static void main(String []args){
+        System.out.println("11");
+    }
+}
